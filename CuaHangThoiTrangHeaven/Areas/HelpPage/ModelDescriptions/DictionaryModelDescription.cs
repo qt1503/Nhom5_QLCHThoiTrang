00ee2547c2f0bf413fe996e1c024c32a6ef1d65e
Tien Le <quytien1503@gmail.com>
2024-10-14 12:41:25 +0700

    Delete CuaHangThoiTrangHeaven directory

--- CuaHangThoiTrangHeaven/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CuaHangThoiTrangHeaven.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- CuaHangThoiTrangHeaven/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CuaHangThoiTrangHeaven.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
